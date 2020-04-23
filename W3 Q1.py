5b95c6275f582a0da28e6db438322631436f231c
SUKESH Bairy <61507903+SukeshBairy@users.noreply.github.com>
2020-04-23 17:46:16 +0530

    Add files via upload

---
+++ W3 Q1.py
@@ -0,0 +1,9 @@
+x=int(input("Enter a number for x-axis"))
+y=int(input("Enter a number for y-axis"))
+z=int(input("Enter a number for z-axis"))
+
+
+
+li=[[["*" for i in range(z)]for j in range(y)]for k in range(z)]
+print(li)
+
